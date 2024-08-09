5b87d6ceda9fcb0ecc624cacd13431f00aaecb96
Shital05 <157453803+shital0503@users.noreply.github.com>
2024-08-09 22:42:10 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+class alpha
+{
+    public static void main (String args[])
+    {
+        char c;
+        for(c='a';c<='z'; ++c)
+        System.out.print(c+"") ;
+    
+}
+}
