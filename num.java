b3ce13418574cea1739f96b24941e74e8faae847
Shital05 <157453803+shital0503@users.noreply.github.com>
2024-08-09 22:58:22 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+class num
+{
+    public static void main(String args[])
+{
+    int i,j;
+    for (i=5;i>=1;i--)
+    {
+         for (j=i;j<=5;j++)
+    
+   {
+           System.out.print(j+"");
+       }
+        System.out.println();
+}
+}
+}
